b9bed79e586903ef869be10ef7949c85f08f290b
ChanoPark <koc081900@korea.ac.kr>
2023-01-27 17:53:24 +0900

    INIT

---
+++ src/main/java/com/rabbit/dayfilm/DayfilmApplication.java
@@ -0,0 +1,13 @@
+package com.rabbit.dayfilm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayfilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayfilmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabbit/dayfilm/DayfilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbit.dayfilm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayfilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
