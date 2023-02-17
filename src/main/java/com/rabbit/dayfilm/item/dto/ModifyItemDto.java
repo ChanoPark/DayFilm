257a6c970d6f4481bf6fffb4e297b03b85355191
daehee <1998opening@gmail.com>
2023-02-17 16:56:37 +0900

    feat : ModifyItemDto Swagger 설정 추가

--- src/main/java/com/rabbit/dayfilm/item/dto/ModifyItemDto.java
+++ src/main/java/com/rabbit/dayfilm/item/dto/ModifyItemDto.java
@@ -1,18 +1,40 @@
 package com.rabbit.dayfilm.item.dto;
 
+import com.rabbit.dayfilm.item.entity.Category;
 import com.rabbit.dayfilm.item.entity.ItemStatus;
 import com.rabbit.dayfilm.item.entity.Method;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 
 @Getter
 public class ModifyItemDto {
+    @ApiModelProperty(value="게시글 제목", example="캐논 ~카메라", required = true)
     private String title;
+
+    @ApiModelProperty(value="카테고리(json)", example="category:{value:'카메라'}", required = true)
+    private Category category;
+
+    @ApiModelProperty(value="게시글 상세 설명", example="캐논 ~카메라는 ~~", required = true)
     private String detail;
+
+    @ApiModelProperty(value="하루 렌탈가격(1일)", example="10000", required = true)
     private Integer pricePerOne;
+
+    @ApiModelProperty(value="하루 렌탈가격(5일 이상)", example="8000", required = true)
     private Integer pricePerFive;
+
+    @ApiModelProperty(value="하루 렌탈가격(10일 이상)", example="5000", required = true)
     private Integer pricePerTen;
+
+    @ApiModelProperty(value="상품 브랜드 명", example="캐논", required = true)
     private String brandName;
+
+    @ApiModelProperty(value="상품 모델 명", example="오토보이 S2", required = true)
     private String modelName;
+
+    @ApiModelProperty(value="대여 방법", example="method:{value:'방문수령'}", required = true)
     private Method method;
+
+    @ApiModelProperty(value="재고", example="5", required = true)
     private Integer quantity;
 }
