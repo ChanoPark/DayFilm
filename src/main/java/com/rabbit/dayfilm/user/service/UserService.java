4785e601fbb903e8d225b6cd1610b9ea91ef9cca
ChanoPark <koc081900@korea.ac.kr>
2023-03-15 23:28:45 +0900

    FEAT: 주문 목록 조회 구현

--- src/main/java/com/rabbit/dayfilm/user/service/UserService.java
+++ src/main/java/com/rabbit/dayfilm/user/service/UserService.java
@@ -1,7 +1,11 @@
 package com.rabbit.dayfilm.user.service;
 
 import com.rabbit.dayfilm.common.CodeSet;
+import com.rabbit.dayfilm.user.dto.OrderListResDto;
+import org.springframework.data.domain.Pageable;
 
 public interface UserService {
     CodeSet checkNickname(String nickname);
+
+    OrderListResDto getOrderList(Long userId, Pageable pageable);
 }
